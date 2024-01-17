71d70837d02c05f3df031225b6ba50dd541a9f4e
vladchashush <vladchashush@gmail.com>
2024-01-17 17:48:45 +0400

    Init project, database, migration, base entity

---
+++ YouDidIT.MVC/YouDidIT.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace YouDidIT.Domain.Entities
+{
+    public class User: Entity
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+}
---
+++ YouDidIT.MVC/YouDidIT.Domain/YouDidItContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YouDidIT.Domain.Entities;
+
+namespace YouDidIT.Domain
+{
+    public class YouDidItContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskApp> Tasks { get; set; }
+
+        public YouDidItContext(DbContextOptions options): base(options)
+        {
+        }
+    }
+}
